3f7eae3a2ea114a44f67fab19d4528ae0d26b346
JEEVARATHINAMR <jeevasmartpkr@gmail.com>
2022-04-16 16:11:25 +0530

    printing Gambling

---
+++ src/com/bridgelabz/Gambling.java
@@ -0,0 +1,36 @@
+package com.bridgelabz;
+
+public class Gambling {
+	
+	public static final int BET_PER_GAME = 1;
+	public static final int STAKE_PER_DAY = 100;
+	
+	public static void main(String[] args) {
+		
+
+        int winnings = 0;
+        int totalWinning = 0;
+
+        while (winnings > -(STAKE_PER_DAY)) {
+
+            if (winnings < 100){
+                byte betResult = (byte) (Math.floor(Math.random() * 10) % 2);
+
+                if (betResult == 1) {
+                    winnings++;
+                    totalWinning = winnings;
+                }
+                else {
+                    winnings--;
+                    totalWinning = winnings;
+                }
+            }
+            else
+                winnings = -(STAKE_PER_DAY);
+
+        }
+        System.out.println("The total winning is " +totalWinning+ "$");
+
+	}
+
+}
